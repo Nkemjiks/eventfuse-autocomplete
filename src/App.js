c9586d821bfbf73b0bc3fa7b2105a64a92486b7c
Blessing Mbonu <mbonunkemjika@ymail.com>
2021-01-11 14:16:37 +0100

    Autocomplete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [searchWord, setSearchWord] = useState("");
+  const [queryResults, setQueryResults] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios({
+        method: "get",
+        url:
+          "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/spelling/AutoComplete",
+        params: { text: searchWord },
+        headers: {
+          "x-rapidapi-key": `${process.env.REACT_APP_RAPID_API_HOST}`,
+          "x-rapidapi-host": `${process.env.REACT_APP_RAPID_API_KEY}`,
+          useQueryString: true,
+        },
+      });
+
+      if (res.data.length > 0) {
+        setQueryResults(res.data);
+      } else {
+        setQueryResults([]);
+        console.log("Sorry, no result marches your enquiry..");
+      }
+    };
+    getData();
+  }, [searchWord]);
+
+  const handleOnchange = (e) => {
+    setSearchWord(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        name="search"
+        value={searchWord}
+        onChange={handleOnchange}
+      />
+      {queryResults.length > 0 &&
+        queryResults.map((data, i) => <p key={i}>{data}</p>)}
     </div>
   );
-}
+};
 
 export default App;
